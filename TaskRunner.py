5e09fc9b6a34f789cfcb1d24d75e170b29d1a700
Adam Slay <aslay@mesonet.org>
2023-11-09 15:31:49 -0600

    refactor to single file

--- TaskRunner.py
+++ TaskRunner.py
@@ -2,6 +2,9 @@ import argparse
 import boto3
 import json
 import logging
+import sys
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', stream=sys.stdout)
 
 
 class TaskRunner:
@@ -20,8 +23,7 @@ class TaskRunner:
         with open(args.config, 'r') as f:
             self.config = json.load(f)
 
-
-    def build_command_arguments(self) -> list:
+    def _build_command_arguments(self) -> list:
         """
         Build the command for the LongTermStats entrypoint from the command line arguments
         :return: command for the LongTermStats entrypoint as a list
@@ -37,7 +39,7 @@ class TaskRunner:
         logging.info(f'Command for LongTermStats entrypoint: {command_args}')
         return command_args
 
-    def build_network_configuration(self) -> dict:
+    def _build_network_configuration(self) -> dict:
         """
         Build the network configuration for the ECS task from SSM parameters
         :return: network configuration for the ECS task as a dict
@@ -61,7 +63,7 @@ class TaskRunner:
             f'Network configuration for ECS task: [subnets: {subnet_id}, security_groups: {security_group_id}, assign_public_ip: {assign_public_ip}]')
         return network_configuration
 
-    def build_overrides(self) -> dict:
+    def _build_overrides(self) -> dict:
         """
         Build the overrides for the ECS task from SSM parameters and command line arguments
         :return: overrides for the ECS task as a dict
@@ -70,7 +72,7 @@ class TaskRunner:
             'containerOverrides': [
                 {
                     'name': self.config['overrides']['container_name'],
-                    'command': self.build_command_arguments(),
+                    'command': self._build_command_arguments(),
                 },
             ],
         }
@@ -84,7 +86,7 @@ class TaskRunner:
         logging.info(f'Container_name: {self.config["overrides"]["container_name"]}')
         return overrides
 
-    def build_tags(self) -> list:
+    def _build_tags(self) -> list:
         """
         Build the tags for the ECS task
         :return: tags for the ECS task as a list of dicts
@@ -107,10 +109,62 @@ class TaskRunner:
             taskDefinition=self.config['task']['task_def'],
             count=self.config['task']['count'],
             launchType=self.config['task']['launch_type'],
-            networkConfiguration=self.build_network_configuration(),
-            overrides=self.build_overrides(),
-            tags=self.build_tags()
+            networkConfiguration=self._build_network_configuration(),
+            overrides=self._build_overrides(),
+            tags=self._build_tags()
         )
         logging.info(f'Task submission response from AWS: {task_response["ResponseMetadata"]["HTTPStatusCode"]}')
         logging.info(f'Task ARN: {task_response["tasks"][0]["taskArn"]}')
         logging.info(f'Task Status: {task_response["tasks"][0]["lastStatus"]}')
+
+
+def parse_args() -> argparse.Namespace:
+    """
+    Parse the command line arguments
+    :return: argparse.Namespace object with the parsed arguments
+    """
+    parser = argparse.ArgumentParser(
+        description='Run LongTermStats as an ECS task in Fargate with the provided arguments(start date, end date, warning level)')
+    parser.add_argument('--sdate', type=str, default=None, help='The start date for the task in YYYY-MM-DD format')
+    parser.add_argument('--edate', type=str, default=None, help='The end date for the task in YYYY-MM-DD format')
+    parser.add_argument('--warn', type=str, default='INFO',
+                        help='Default=INFO. The warning level to log (DEBUG, INFO, WARN, ERROR, CRITICAL)')
+    parser.add_argument('--config', type=str, default='config.json', help='Path to the config file')
+    parser.add_argument('--tags', type=str, default=None,
+                        help='Tags to apply to the task. Comma separated and no spaces "key1=value1,key2=value2"')
+    args = parser.parse_args()
+
+    if args.warn not in ['DEBUG', 'INFO', 'WARN', 'ERROR', 'CRITICAL']:
+        raise ValueError(f'Invalid warning level: {args.warn}')
+    else:
+        logging.getLogger().setLevel(args.warn)
+
+    if args.tags is not None:
+        args.tags = format_tags(args.tags)
+    return args
+
+
+def format_tags(tags: str) -> list:
+    """
+    Parse the tags argument into a list of dicts
+    :param tags: tags argument from the command line
+    :return: list of dicts with the tags formatted as required by the boto3 API
+    """
+    tags = tags.split(',')  # ['key1=value1', 'key2=value2']
+    tags = [tag.split('=') for tag in tags]  # [['key1', 'value1'], ['key2', 'value2']]
+    tags = [{'key': tag[0], 'value': tag[1]} for tag in
+            tags]  # [{'key': 'key1', 'value': 'value1'}, {'key': 'key2', 'value': 'value2'}]
+    return tags
+
+
+def main():
+    args = parse_args()
+    runner = TaskRunner(args)
+    runner.submit_task()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        logging.exception(f'And unexpected error occurred: {e}')
